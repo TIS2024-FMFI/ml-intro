a039b0261225938f1aef5853ea4cf61cf8fc1844
kuboo1 <kovac329@uniba.sk>
2025-01-10 09:06:17 +0100

    Buffer inicializácia v Edge
    Pridal som inicializáciu pre buffer

--- ml-intro/src/renderer/Edge.cpp
+++ ml-intro/src/renderer/Edge.cpp
@@ -14,3 +14,31 @@ Edge::Edge(vec3 posA, vec3 posB, float w) : posA(posA), posB(posB), weight(w) {
     vec3 p = mix(posA, posB, 0.25);
     Label(p, weight);
 }
+
+void Edge::InitializeBuffers() {
+    if (initialized) return;
+
+    glGenVertexArrays(1, &edgeVAO);
+    glGenBuffers(1, &edgeVBO);
+
+    glBindVertexArray(edgeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, edgeVBO);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0); // pos
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); // weight
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(4 * sizeof(float))); // UV
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    Edge::shaderProgram = ShaderPrograms::getInstance().getEdgeShaderProgramId();
+
+    Label::InitializeBuffers();
+
+    initialized = true;
+}
